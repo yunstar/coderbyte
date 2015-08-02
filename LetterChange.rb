0ec10f5be143645d07b77e9a34cc0b5626868a96
yunstar <yunstar1105@gmail.com>
2015-08-02 11:59:16 +0900

    Create LetterChange.rb

---
+++ LetterChange.rb
@@ -0,0 +1,11 @@
+def LetterChanges(str)
+  
+ str=str.tr('a-y','b-z').tr('z','a').tr('by','BY')
+  # code goes here
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterChanges(STDIN.gets)           
